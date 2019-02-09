14189279204f5978383ae7377d1bf2a462589539
Christopher Alessandro <calessandro0827@gmail.com>
2019-02-08 17:54:36 -0700

    Updated setup_env script to include packages for building

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -3,6 +3,10 @@
 echo "Getting compiler.."
 wget -c https://releases.linaro.org/components/toolchain/binaries/6.4-2018.05/arm-linux-gnueabihf/gcc-linaro-6.4.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz
 echo "extracting compiler.."
+mkdir tools
 tar xf gcc-linaro-6.4.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz -C tools
 rm gcc-linaro-6.4.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz
+
+echo "Installing tools.."
+sudo apt install build-essential bc bison flex libncurses-dev libssl-dev debootstrap qemu-user-static -y
 echo "done.."
