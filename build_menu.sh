a63350cc89f850cbe8e0a574b391916132e4aad8
Christopher Alessandro <calessandro0827@gmail.com>
2019-02-10 19:38:32 -0700

    Add chmod to overlays script

--- build_menu.sh
+++ build_menu.sh
@@ -15,6 +15,7 @@ setup_env () {
 	chmod +x scripts/build_u-boot.sh
 	chmod +x scripts/build_kernel.sh
 	chmod +x scripts/build_debian-rootfs.sh
+	chmod +x scripts/build_overlays.sh
 	chmod +x scripts/make-image.sh
 	sh scripts/setup_env.sh
 }
