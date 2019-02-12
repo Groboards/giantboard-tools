4ce345baf6bc522a38a05e2a5cd7ab22ade9169f
Christopher Alessandro <calessandro0827@gmail.com>
2019-02-11 17:18:33 -0700

    Update u-boot build script
    Moved build directory into output/build/u-boot
    
    Setup output folder for u-boot.bin and place there when build is complete

--- scripts/build_u-boot.sh
+++ scripts/build_u-boot.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-export CC=`pwd`/tools/gcc-linaro-6.4.1-2018.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
-echo "Downloading u-boot"
-git clone https://github.com/u-boot/u-boot
-cd u-boot/
-git checkout v2019.01 -b tmp
-echo "getting patches"
-wget -c https://github.com/eewiki/u-boot-patches/raw/master/v2019.01/0001-ARM-at91-Convert-SPL_GENERATE_ATMEL_PMECC_HEADER-to-.patch
-echo "patching.."
-cp ../patches/u-boot/at91-sama5d27_giantboard.dts arch/arm/dts/
-cp ../patches/u-boot/sama5d27_giantboard.dtsi arch/arm/dts/
-cp ../patches/u-boot/sama5d27_giantboard_mmc_defconfig configs/
+# Build variables
+patch_dir="$(pwd)/patches/u-boot"
+output_dir="$(pwd)/output"
+build_dir="${output_dir}/build"
+uboot_bin="${output_dir}/u-boot"
+uboot_dir="${build_dir}/u-boot"
 
-patch -p1 < 0001-ARM-at91-Convert-SPL_GENERATE_ATMEL_PMECC_HEADER-to-.patch
-patch -p1 < ../patches/u-boot/giantboard-fixes.patch
+# specify compiler 
+CC=`pwd`/tools/gcc-linaro-6.4.1-2018.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+
+echo "making output dir."
+# Make the u-boot output dir
+mkdir -p "${uboot_bin}"
+
+# clone u-boot
+git -C ${build_dir} clone https://github.com/u-boot/u-boot
+
+echo "patching.."
+
+cp patches/u-boot/at91-sama5d27_giantboard.dts ${uboot_dir}/arch/arm/dts/
+cp patches/u-boot/sama5d27_giantboard.dtsi ${uboot_dir}/arch/arm/dts/
+cp patches/u-boot/sama5d27_giantboard_mmc_defconfig ${uboot_dir}/configs/
+
+patch -d ${uboot_dir} -p1 < patches/u-boot/giantboard-fixes.patch 
 
 echo "patches complete.."
 echo "starting u-boot build.."
-make ARCH=arm CROSS_COMPILE=${CC} distclean
-make ARCH=arm CROSS_COMPILE=${CC} sama5d27_giantboard_mmc_defconfig
-make ARCH=arm CROSS_COMPILE=${CC}
+make -C ${uboot_dir} ARCH=arm CROSS_COMPILE=${CC} distclean
+make -C ${uboot_dir} ARCH=arm CROSS_COMPILE=${CC} sama5d27_giantboard_mmc_defconfig
+make -C ${uboot_dir} ARCH=arm CROSS_COMPILE=${CC}
+
+cp -v ${uboot_dir}/u-boot.bin ${uboot_bin}
+
 echo "finished building u-boot"
