541f61b458f4379e7cae6dc5e4a1aff05f48e972
Christopher Alessandro <calessandro0827@gmail.com>
2019-02-11 18:20:30 -0700

    Rename overlays dir and add default uEnv.txt

--- scripts/build_overlays.sh
+++ scripts/build_overlays.sh
@@ -1,10 +1,12 @@
-#!/bin/bash                                                                                                                                 
+#!/bin/bash                                                                                                                              
+# restart script with root privileges if not already
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@" ]
 
 patch_dir="$(pwd)/patches/overlays"
 output_dir="$(pwd)/output"
-firmware_dir="${output_dir}/firmware"
+overlays_dir="${output_dir}/overlays"
 
-mkdir -p "${firmware_dir}"
+mkdir -p "${overlays_dir}"
 
 # overlays array
 declare -a overlays=("GB-22LCD-FEATHERWING"
@@ -14,7 +16,7 @@ declare -a overlays=("GB-22LCD-FEATHERWING"
 # Install all the libs listed in the blinka_libs array
 for i in "${overlays[@]}"
 do
-	dtc -@ -I dts -O dtb -o ${firmware_dir}/"$i".dtbo ${patch_dir}/"$i".dts
+	dtc -@ -I dts -O dtb -o ${overlays_dir}/"$i".dtbo ${patch_dir}/"$i".dts
 done
 
 echo "done building.."
