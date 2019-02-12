f5ca5cf8e28c99ce926186ef020569672fc8cb16
Christopher Alessandro <calessandro0827@gmail.com>
2019-02-11 17:19:25 -0700

    Update build_at91bootstrap.sh
    Moved build directory into output/build/at91bootstrap
    
    Setup output folder for at91boostrap bin and place there when build is complete

--- scripts/build_at91bootstrap.sh
+++ scripts/build_at91bootstrap.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-echo "exporting compilering.."
-export CC=`pwd`/tools/gcc-linaro-6.4.1-2018.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
-echo "Downloading the latest at91bootstrcap"
-git clone https://github.com/linux4sam/at91bootstrap
-cd at91bootstrap/
-git checkout v3.8.10 -b tmp
+# Build variables
+output_dir="$(pwd)/output"
+build_dir="${output_dir}/build"
+at91boot_bin="${output_dir}/at91bootstrap"
+at91boot_dir="${build_dir}/at91bootstrap"
+
+# specify compiler
+CC=`pwd`/tools/gcc-linaro-6.4.1-2018.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+
+echo "making output dir."
+# Make the at91bootstrap output dir
+mkdir -p "${at91boot_bin}"
+
+echo "Downloading the latest at91bootstrap"
+git -C ${build_dir} clone https://github.com/linux4sam/at91bootstrap
+git -C ${at91boot_dir} checkout v3.8.10 -b tmp
 echo "building at91bootstrap"
-make ARCH=arm CROSS_COMPILE=${CC} distclean
-make ARCH=arm CROSS_COMPILE=${CC} sama5d27_som1_eksd1_uboot_defconfig
-make ARCH=arm CROSS_COMPILE=${CC}
+make -C ${at91boot_dir} ARCH=arm CROSS_COMPILE=${CC} distclean
+make -C ${at91boot_dir} ARCH=arm CROSS_COMPILE=${CC} sama5d27_som1_eksd1_uboot_defconfig
+make -C ${at91boot_dir} ARCH=arm CROSS_COMPILE=${CC}
+
+# copy the built bin to the output
+cp -v ${at91boot_dir}/binaries/sama5d27_som1_ek-sdcardboot-uboot-3.8.10.bin ${at91boot_bin}/BOOT.BIN
+
 echo "finished building at91bootstrap"
