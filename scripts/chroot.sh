d56afcfc88220bab7e21061d846ebbf551b5acd5
Christopher Alessandro <calessandro0827@gmail.com>
2019-03-01 10:34:42 -0700

    Add folder for battery serivce and chmod the grow_sd.sh script

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -90,6 +90,9 @@ sh -c "echo '127.0.0.1       giantboard' >> /etc/hosts"
 chmod +x /usr/bin/batt_service.sh
 systemctl enable batt.service
 
+# Make grow_sd.sh executable
+chmod +x /usr/bin/grow_sd.sh
+
 # enable usb getty
 systemctl enable getty@ttyGS0.service
 
