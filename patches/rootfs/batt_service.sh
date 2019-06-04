59a46ebffaa880216efcd4807cb5a07eaddecce7
Christopher Alessandro <calessandro0827@gmail.com>
2019-06-04 12:56:30 -0700

    Update batt_service.sh
    With I2C0 disabled, I2C1 becomes I2C0. This update trys both interfaces to disable battery charging.

--- patches/rootfs/batt_service.sh
+++ patches/rootfs/batt_service.sh
@@ -3,6 +3,15 @@ boot_arg=giantboard.disable_charging
 
 if grep -iq "${boot_arg}=[a-zA-Z0-9]" /proc/cmdline
 then
+    sudo i2cset -f -y 0 0x5b 0x71 129
+    echo "Battery charging disabled"
+else
+    sudo i2cset -f -y 0 0x5b 0x71 0x00
+    echo "Battery charging enabled"
+fi
+
+if grep -iq "${boot_arg}=[a-zA-Z0-9]" /proc/cmdline
+then
     sudo i2cset -f -y 1 0x5b 0x71 129
     echo "Battery charging disabled"
 else
