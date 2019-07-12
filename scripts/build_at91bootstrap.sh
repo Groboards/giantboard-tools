a6c49f96744f5777a79d1e5ee9ec0dc2e0e5af03
Christopher Alessandro <calessandro0827@gmail.com>
2019-07-12 11:58:07 -0700

    Add custom config to at91bootstrap and update to latest release version.
    Update script to pull latest release version of at91bootstrap 3.8.13
    
    Add custom config with flag to add support for Backup-Refresh mode for power management:
    * CONFIG_BACKUP_MODE=y

--- scripts/build_at91bootstrap.sh
+++ scripts/build_at91bootstrap.sh
@@ -3,6 +3,7 @@
 # Build variables
 output_dir="$(pwd)/output"
 build_dir="${output_dir}/build"
+patch_dir="$(pwd)/patches/at91bootstrap"
 at91boot_bin="${output_dir}/at91bootstrap"
 at91boot_dir="${build_dir}/at91bootstrap"
 
@@ -15,13 +16,17 @@ mkdir -p "${at91boot_bin}"
 
 echo "Downloading the latest at91bootstrap"
 git -C ${build_dir} clone https://github.com/linux4sam/at91bootstrap
-git -C ${at91boot_dir} checkout v3.8.10 -b tmp
+git -C ${at91boot_dir} checkout v3.8.13 -b tmp
 echo "building at91bootstrap"
+
+# copy config
+cp ${patch_dir}/sama5d27_giantboard_sd1_uboot_defconfig ${at91boot_dir}/board/sama5d27_som1_ek/
+
 make -C ${at91boot_dir} ARCH=arm CROSS_COMPILE=${CC} distclean
-make -C ${at91boot_dir} ARCH=arm CROSS_COMPILE=${CC} sama5d27_som1_eksd1_uboot_defconfig
+make -C ${at91boot_dir} ARCH=arm CROSS_COMPILE=${CC} sama5d27_giantboard_sd1_uboot_defconfig
 make -C ${at91boot_dir} ARCH=arm CROSS_COMPILE=${CC}
 
 # copy the built bin to the output
-cp -v ${at91boot_dir}/binaries/sama5d27_som1_ek-sdcardboot-uboot-3.8.10.bin ${at91boot_bin}/BOOT.BIN
+cp -v ${at91boot_dir}/binaries/sama5d27_som1_ek-sdcardboot-uboot-3.8.13.bin ${at91boot_bin}/BOOT.BIN
 
 echo "finished building at91bootstrap"
