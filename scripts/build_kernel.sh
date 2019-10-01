dd4e5a379795b6aac5b435949e34d79237ce5f86
Christopher Alessandro <calessandro0827@gmail.com>
2019-10-01 11:58:21 -0700

    Update build_kernel.sh
    Update kernel version to 5.3

--- scripts/build_kernel.sh
+++ scripts/build_kernel.sh
@@ -15,7 +15,7 @@ cross_make="make -C ${linux_dir} ARCH=arm CROSS_COMPILE=${CC}"
 
 # TODO: make these user-specifiable defaults
 patches=""
-release="${release:-v5.0}"
+release="${release:-v5.3}"
 
 echo "Building kernel release: ${release}"
 mkdir -p "${build_dir}"
