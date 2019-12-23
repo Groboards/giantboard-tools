ddb115a21d405e51f3c09d0f4c9107731d94e732
Christopher Alessandro <calessandro0827@gmail.com>
2019-12-23 14:07:31 -0700

    Add multicore compile

--- scripts/build_u-boot.sh
+++ scripts/build_u-boot.sh
@@ -7,6 +7,9 @@ build_dir="${output_dir}/build"
 uboot_bin="${output_dir}/u-boot"
 uboot_dir="${build_dir}/u-boot"
 
+# core count for compiling with -j
+cores=$(( $(nproc) * 2 ))
+
 release="${release:-v2019.07}"
 
 # specify compiler 
@@ -19,18 +22,19 @@ mkdir -p "${uboot_bin}"
 # clone u-boot
 git -C ${build_dir} clone https://github.com/u-boot/u-boot
 git -C ${uboot_dir} checkout ${release} -b tmp
+
 echo "patching.."
 
 cp patches/u-boot/at91-sama5d27_giantboard.dts ${uboot_dir}/arch/arm/dts/
 cp patches/u-boot/sama5d27_giantboard_mmc_defconfig ${uboot_dir}/configs/
-
 patch -d ${uboot_dir} -p1 < patches/u-boot/giantboard-fixes.patch 
 
 echo "patches complete.."
+
 echo "starting u-boot build.."
-make -C ${uboot_dir} ARCH=arm CROSS_COMPILE=${CC} distclean
-make -C ${uboot_dir} ARCH=arm CROSS_COMPILE=${CC} sama5d27_giantboard_mmc_defconfig
-make -C ${uboot_dir} ARCH=arm CROSS_COMPILE=${CC}
+make -j"${cores}" -C ${uboot_dir} ARCH=arm CROSS_COMPILE=${CC} distclean
+make -j"${cores}" -C ${uboot_dir} ARCH=arm CROSS_COMPILE=${CC} sama5d27_giantboard_mmc_defconfig
+make -j"${cores}" -C ${uboot_dir} ARCH=arm CROSS_COMPILE=${CC}
 
 cp -v ${uboot_dir}/u-boot.bin ${uboot_bin}
 
