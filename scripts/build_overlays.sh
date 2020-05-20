07573471e9766b3318a7d4ef26bcc56145390474
Christopher Alessandro <calessandro0827@gmail.com>
2020-05-19 21:00:37 -0700

    Add Adafruit 3.5" LCD featherwing

--- scripts/build_overlays.sh
+++ scripts/build_overlays.sh
@@ -9,20 +9,21 @@ overlays_dir="${output_dir}/overlays"
 mkdir -p "${overlays_dir}"
 
 # overlays array
-declare -a overlays=("GB-24LCD-FEATHERWING"
-					 "GB-ETHERNET-FEATHERWING"
-					 "GB-I2C0"
-					 "GB-I2S0"
-					 "GB-I2S0-NO-MCK"
-					 "GB-I2S0-NO-MCK-NO-DI"
-					 "GB-PWM1"
-					 "GB-PWM1-3"
-					 "GB-PWM2"
-					 "GB-SPI0-ENC28J60"
-					 "GB-SPI0-SPIDEV-CS-PWML1"
-					 "GB-SPI0-SPIDEV-NO-CS"
-					 "GB-WIFI-FEATHERWING"
-					 "GB-UART2-FLX4-AD2-AD3")
+declare -a overlays=("GB-24LCD-FEATHERWING" 
+					"GB-35LCD-FEATHERWING"
+					"GB-ETHERNET-FEATHERWING"
+					"GB-I2C0"
+					"GB-I2S0"
+					"GB-I2S0-NO-MCK"
+					"GB-I2S0-NO-MCK-NO-DI"
+					"GB-PWM1"
+					"GB-PWM1-3"
+					"GB-PWM2"
+					"GB-SPI0-ENC28J60"
+					"GB-SPI0-SPIDEV-CS-PWML1"
+					"GB-SPI0-SPIDEV-NO-CS"
+					"GB-WIFI-FEATHERWING"
+					"GB-UART2-FLX4-AD2-AD3")
 
 # Install all the libs listed in the blinka_libs array
 #dtc -@ -I dts -O dtb -o ${overlays_dir}/"$i".dtbo ${patch_dir}/"$i".dts
