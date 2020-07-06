053c8d65bbd3b56eb5f73bdfbd32c111ae312b18
Christopher Alessandro <calessandro0827@gmail.com>
2020-07-06 16:47:54 -0700

    Enable the dns resolver service. For some reason its disabled in the debian 10 default rootfs.

--- scripts/chroot_min.sh
+++ scripts/chroot_min.sh
@@ -60,6 +60,9 @@ deb http://security.debian.org/debian-security buster/updates main contrib
 deb-src http://security.debian.org/debian-security buster/updates main contrib
 EOT
 
+# Enable the dns resolver service. For some reason its disabled in the debian 10 default rootfs
+systemctl enable systemd-resolved.service
+
 # Update sources
 echo "Log: (chroot_min) apt-get udpating rootfs packages."
 apt-get update
